8638a11025b0983ad9af3d0bb3d624e6891b7d54
Kianayana <kianafillius@gmail.com>
2023-04-08 19:28:26 -0700

    Created Basic CSS file

---
+++ VSC_Test/Hello_World.js
@@ -0,0 +1,9 @@
+var greeting = 'Hello, World';
+    console.log(greeting);
+
+var greeting2 = "Welcome to Node.js";
+    console.log(greeting2);
+
+var sentence1 = 'Learning how to debig code with the debugger';
+    console.log(sentence1);
+
